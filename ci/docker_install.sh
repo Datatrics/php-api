bd281d98f57c375aa887cb14277497959d3ec21f
Nico Borghuis <nico@datatrics.com>
2017-04-27 11:50:05 +0200

    composer update

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -14,8 +14,24 @@ curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.
 chmod +x /usr/local/bin/phpunit
 
 #install composer
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-composer install -o
+curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
+
+
+EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+
+php composer.phar install -o
 
 # Install mysql driver
 # Here you can install any other extension that you need
