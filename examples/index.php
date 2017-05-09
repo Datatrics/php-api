a153fb80c7e1a56d8a5fdcf9b0ff0a8cae0f03e5
Nico Borghuis <nico@datatrics.com>
2017-05-09 20:10:43 +0200

    Test update

--- examples/index.php
+++ examples/index.php
@@ -2,35 +2,40 @@
 
 require_once(__DIR__."/../vendor/autoload.php");
 
-$apiKey = 'cda6147aecdd40a7ba413db94fbf2742';
-$projectId = '255522';
+$apiKey = '';
+$projectId = '';
 
 $api = new Datatrics\API\Client($apiKey, $projectId);
-$project = $api->Project->Get('255522');
-#foreach($projects['items'] as $project) {
-    echo "<pre>";
-    print_r($project);
-    echo "</pre>";
-    if ($project['projectid']) {
-        #$api->SetProjectId($project['projectid']);
-        $profile = array(
-
-        );
-        $profileCreate = array(
-            "projectid" => $projectId,
-            "profileid" => "995",
-            "source" => "Datatrics",
-            "profile" => [
-                "firstname" => "N",
-                "lastname" => "B",
-                "zip" => "1234AB",
-                "city" => "Plaats"
-            ]
-        );
-        $profileCreate = $api->Profile->Create($profileCreate);
-        echo "<pre>";
-        print_r($profileCreate);
-        echo "</pre>";
-    }
-    exit;
-#}
+$profileCreate = array(
+    "projectid" => $projectId,
+    "profileid" => "1",
+    "source" => "Datatrics",
+    "profile" => [
+        "firstname" => "N",
+        "lastname" => "B",
+        "zip" => "1234AB",
+        "city" => "Plaats"
+    ]
+);
+$profileCreate = $api->Profile->Create($profileCreate);
+echo "<pre>";
+print_r($profileCreate);
+echo "</pre>";
+$contentCreate = array(
+    "projectid" => $projectId,
+    "itemid" => "1",
+    "source" => "Datatrics",
+    "type" => 'item',
+    'itemtype' => 'custom',
+    "item" => [
+        "name" => "N",
+        "description" => "B",
+        "url" => "https://www.datatrics.com",
+        "image" => "https://www.datatrics.com/image.png"
+    ]
+);
+$contentCreate = $api->Content->Create($contentCreate);
+echo "<pre>";
+print_r($contentCreate);
+echo "</pre>";
+exit;
